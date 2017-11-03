2ce5f1ab9d3f42c1d3c9c4db48d023ff0bc88a1e
brigh542 <31809756+brigh542@users.noreply.github.com>
2017-11-03 14:48:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-//#include <sys/types.h>
-//#include <sys/stat.h>
 #define BUF 10000
 
 #include "minix.h"
@@ -29,7 +27,7 @@ int main(){
 
     userInput = (char *)calloc(BUF, 1);
 
-    write(1, "minix>>",7);
+    write(1, "minix:",6);
     read(0, userInput, BUF);
 
     //use string compare to
