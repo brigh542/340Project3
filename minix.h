0b29145c08c39992b425d030ab7a70319ccffced
brigh542 <31809756+brigh542@users.noreply.github.com>
2017-11-03 14:44:31 -0400

    Create minix.h

---
+++ minix.h
@@ -0,0 +1,37 @@
+#ifndef MINIX_H_
+#define MINIX_H_
+
+
+//structure
+
+struct super_block{
+    unsigned short s_ninodes;
+    unsigned short s_nzones;
+    unsigned short s_imap_blocks;
+    unsigned short s_zmap_blocks;
+    unsigned short s_firstdatazone;
+    unsigned short s_log_zone_size;
+    unsigned int s_maxsize;
+    unsigned short s_magic;
+    unsigned short s_state;
+    unsigned int s_zones;
+};
+
+struct inode{
+  unsigned short i_mode;
+  unsigned short i_uid;
+  unsigned int i_size;
+  unsigned int i_time;
+  unsigned char i_gid;
+  unsigned char i_nlinks;
+  unsigned short i_zone[9];
+};
+
+struct dir_entry {
+    unsigned short inode;
+    char name[0];
+
+};
+
+
+#endif
