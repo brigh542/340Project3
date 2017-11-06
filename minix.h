9facf18f4cb1f456a029826e27137c059779b3e2
brigh542 <31809756+brigh542@users.noreply.github.com>
2017-11-05 21:49:20 -0500

    Add files via upload

--- minix.h
+++ minix.h
@@ -1,46 +1,75 @@
+
 #ifndef MINIX_H_
 #define MINIX_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
+#include <string.h>
 
-//structure
+#define DAYS_PER_4YEARS 1461
 
-struct super_block{
-    unsigned short s_ninodes;
-    unsigned short s_nzones;
-    unsigned short s_imap_blocks;
-    unsigned short s_zmap_blocks;
-    unsigned short s_firstdatazone;
-    unsigned short s_log_zone_size;
-    unsigned int s_maxsize;
-    unsigned short s_magic;
-    unsigned short s_state;
-    unsigned int s_zones;
+#define SECONDS_PER_DAY 86400
+
+
+
+//define global variables!
+
+extern char* image;
+extern int mounted;
+
+//declare structures!
+
+struct minix_super_block {
+	unsigned short s_ninodes;
+	unsigned short s_nzones;
+	unsigned short s_imap_blocks;
+	unsigned short s_zmap_blocks;
+	unsigned short s_firstdatazone;
+	unsigned short s_log_zone_size;
+	unsigned int s_max_size;
+	unsigned short s_magic;
+	unsigned short s_state;
+	unsigned int s_zones;
 };
 
-struct inode{
-  unsigned short i_mode;
-  unsigned short i_uid;
-  unsigned int i_size;
-  unsigned int i_time;
-  unsigned char i_gid;
-  unsigned char i_nlinks;
-  unsigned short i_zone[9];
+struct minix_inode
+{
+	unsigned short i_mode;
+	unsigned short i_uid;
+	unsigned int i_size;
+	unsigned int i_time;
+	unsigned char i_gid;
+	unsigned char i_nlinks;
+	unsigned short i_zone[9];
 };
 
-struct dir_entry {
-    unsigned short inode;
-    char name[0];
-
+struct minix_dir_entry
+{
+	unsigned short inode; 
+	char name[0]; 		
 };
 
-
-void showsuper();
 void help();
-void quit();
-void traverse(int fd, int detailPrint);
-void showzone(int fd, int zone);
-void minimount();
-void miniumount();
-
-
+void showsuper();
+void traverse(int showMore);
+void showzone(char* zoneNumber);
+void showfile(char* fileName);
+/*
+//helper functions
+void itoa(char *s, int x);
+void processImode(int a, char* returnString);
+void processPermissionBytes(char* permissionBytes, char* returnString);
+void convertTime(int seconds, char* returnString);
+void hexDump (void *addr, int len);
+void dtoh(char* returnString, int d);
+char* strrev(char *str);
+char* negateHex(char* hex);
+*/
 #endif
+
